2818a6b612af4315ac857a27822248aa7278eb05
颢珑 张 <edwardazazel@126.com>
2018-07-09 22:22:41 -0400

    header of singup page created

--- practice_project/src/index.js
+++ practice_project/src/index.js
@@ -4,5 +4,35 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+/**
+ * 
+ * props
+ * - page_title
+ *   the string displays at the center of the header section
+ */
+function Header(props){
+    return(
+        <div className="Header">
+            <div className="Title">
+                {props.page_title}
+            </div>
+        </div>
+    )
+}
+
+const header = {
+    page_title: 'Signup'
+};
+var element = React.createElement('h1', { className: 'greeting' }, 'Hello, world!');
+
+
+ReactDOM.render(
+    <Header 
+        page_title = {header.page_title}
+    />, 
+    document.getElementById('root')
+);
+
+
 registerServiceWorker();
